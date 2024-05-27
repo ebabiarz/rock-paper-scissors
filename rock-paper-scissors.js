cbef911651ce0927492285bf53ab8379ac285bcf
Eric Babiarz <145168964+ebabiarz@users.noreply.github.com>
2024-05-27 16:36:26 -0500

    Add logic for receiving the user's choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,28 +1,45 @@
-// Run function to determine what the computer chooses
+let humanScore = 0
+let computerScore = 0
+getComputerChoice();
+getHumanChoice();
+
+
+// Function that determines computer choice
 function getComputerChoice() {
     // Generate integer between 0 and 2. Each possible integer results in a different choice.
-    function getRandomInt(max) {
-        let choice = Math.floor(Math.random() * max);
-        if (choice === 0) {
+    function getRandomInt() {
+        let computerChoice = Math.floor(Math.random() * 3);
+        if (computerChoice === 0) {
             return "Rock";
-        } else if (choice === 1) {
+        } else if (computerChoice === 1) {
             return "Paper";
         } else {
             return "Scissors";
         }
     }
     // Write computer choice to the console.
-    console.log(getRandomInt(3));
+    console.log(getRandomInt());
 }
 
-// Run getComputerChoice function
-getComputerChoice();
+// Function that takes user choice
+function getHumanChoice() {
+    function humanChoicePrompt() {
+        let userChoice = prompt("Rock, Paper, or Scissors?");
+        if (userChoice.toLowerCase() === "rock") {
+            return "Rock";
+        } else if (userChoice.toLowerCase() === "paper") {
+            return "Paper";
+        } else if (userChoice.toLowerCase() === "scissors") {
+            return "Scissors";
+        } else {
+            return "Invalid Choice";
+        }
+    }
+    console.log(humanChoicePrompt());
+}
 
 
 
 /*
-Randomly generate a number
-If the number is 0, return rock
-If the number is 1, return paper
-If the number is 3, return scissors
+
 */
